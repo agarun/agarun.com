79aed6c53c6018dd1517d813c3340848ba13804c
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-08-21 21:20:28 -0400

    Create custom document

---
+++ pages/_document.js
@@ -0,0 +1,61 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+import { GA_TRACKING_ID } from '../lib/gtag';
+import { version } from '../package.json';
+
+function GoogleAnalyticsScript() {
+  return (
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_TRACKING_ID}', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
+    </>
+  );
+}
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <GoogleAnalyticsScript />
+
+          <meta name="theme-color" content="#000000" />
+          <meta name="msapplication-TileColor" content="#000000" />
+
+          <meta name="build version" content={version} />
+          <meta name="description" content="Aaron Agarunov's portfolio" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
