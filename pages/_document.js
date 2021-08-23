dbe1d22ba2e80cf0a029ea05e3b3e590a7272551
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-08-23 18:23:18 -0400

    Add favicon tags to document

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,31 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" href="/favicons/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#0e0629"
+          />
+
           <meta name="theme-color" content="#0e0629" />
           <meta name="msapplication-TileColor" content="#0e0629" />
 
