b9623003d22e44456f7a40cee2a96e2121354197
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-08-23 18:06:19 -0400

    Speed up Google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,17 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="theme-color" content="#000000" />
-          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#0e0629" />
+          <meta name="msapplication-TileColor" content="#0e0629" />
 
           <meta name="build version" content={packageInfo.version} />
           <meta name="description" content="Aaron Agarunov's portfolio" />
 
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
