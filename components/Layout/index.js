a81dfa2ea39e67282154782043f48aada3d07223
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-01 14:53:57 -0400

    Remove layout's <main> and <header> on the root page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,28 @@
+import { useRouter } from 'next/router';
+import { NavLink } from '../Nav';
+import Nav from '../Nav';
 import * as styles from './styles';
 
 function Layout({ children }) {
+  const router = useRouter();
+  const isRootPage = router.pathname.length <= 1;
+
+  if (isRootPage) {
+    return <div css={styles.container}>{children}</div>;
+  }
+
   return (
-    <main role="main" css={styles.main}>
-      {children}
-    </main>
+    <div css={styles.container}>
+      <header css={styles.header}>
+        <NavLink href="/">
+          <h1 css={styles.title}>Aaron Agarunov</h1>
+        </NavLink>
+        <Nav />
+      </header>
+      <main role="main" css={styles.main}>
+        {children}
+      </main>
+    </div>
   );
 }
 
--- components/Layout/styles.js
+++ components/Layout/styles.js
@@ -1,8 +1,21 @@
 import { css } from '@emotion/react';
 
-export const main = css`
+export const container = css`
   position: relative;
   margin: 0 auto;
   width: 960px;
   max-width: var(--layout-width);
 `;
+
+export const title = css`
+  font-size: calc(var(--font-size-scale) * 26px);
+`;
+
+export const header = css`
+  padding-top: calc(var(--spacing) * 10);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const main = css``;
