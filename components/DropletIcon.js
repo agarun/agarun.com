4744752643681f6a6db0c6a6f1b98fd4e34e8104
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-02 15:00:28 -0400

    Add droplet icon for ThemeSwitch

---
+++ components/DropletIcon.js
@@ -0,0 +1,18 @@
+function DropletIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      role="img"
+      aria-label="change theme"
+      {...props}
+    >
+      <title>change theme</title>
+      <path d="M12 2.69l5.66 5.66a8 8 0 11-11.31 0z" />
+    </svg>
+  );
+}
+
+export default DropletIcon;
