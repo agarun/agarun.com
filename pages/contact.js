71ab19e166f252ebc7cf69b0a3c117fef24799c8
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-02 14:59:52 -0400

    Add entry animations for contact page links

--- pages/contact.js
+++ pages/contact.js
@@ -1,69 +1,111 @@
+import { useState } from 'react';
+import { css } from '@emotion/react';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { getSocials } from '../lib/socials';
+
 export async function getStaticProps() {
-  const links = [
-    [
-      {
-        title: 'email',
-        href: 'mailto:agarunov.aaron@gmail.com',
-        copy: true,
-        color: '#ea4335',
-      },
-      {
-        title: 'github',
-        href: 'https://github.com/agarun',
-        copy: false,
-        color: '#333333',
-      },
-      {
-        title: 'linkedin',
-        href: 'https://linkedin.com/in/agarunov/',
-        color: '#0077b5',
-      },
-      {
-        title: 'twitter',
-        href: 'https://twitter.com/agarune',
-        color: '#1da1f2',
-      },
-      {
-        title: 'instagram',
-        href: 'https://www.instagram.com/agarunov',
-        color: '#833ab4',
-      },
-      {
-        title: 'RÉSUMÉ',
-        href: 'https://agarun.com/AaronAgarunovResume.pdf',
-        color: '#171079',
-      },
-      {
-        title: 'lastfm',
-        href: 'http://last.fm/user/aeroecho',
-        description: "music I'm listening to",
-        color: '#d51007',
-      },
-      {
-        title: 'whatpulse',
-        href: 'https://whatpulse.org/intentional',
-        description: "how many keys and clicks I've made",
-        color: '#58607c',
-      },
-      {
-        title: 'letterboxd',
-        href: 'https://letterboxd.com/agarunov/films/',
-        description: 'film diary',
-        color: '#00e054',
-      },
-      {
-        title: 'keybase',
-        href: 'https://keybase.io/agarun',
-        description: 'secret messages',
-        color: '#4c8eff',
-      },
-    ],
-  ];
-  return { props: { links } };
+  return { props: { links: getSocials().filter((item) => !item.disabled) } };
 }
 
+const styles = {
+  list: css`
+    margin: calc(var(--spacing) * 8) 0;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: flex-start;
+  `,
+  listItem: ({ isCurrentLink, linkColor }) => css`
+    position: relative;
+    font-size: calc(var(--font-size-scale) * 64px);
+    line-height: 1.2;
+    a {
+      text-decoration: none;
+      font-weight: 700;
+      color: ${isCurrentLink || !linkColor
+        ? 'var(--colors-text-primary)'
+        : linkColor};
+      transition: color 200ms ease-out;
+    }
+  `,
+  listCover: css`
+    position: absolute;
+    top: 0;
+    left: calc(-100% - var(--spacing) * 15);
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: var(--colors-background);
+    content: '';
+    pointer-events: none;
+    z-index: 1;
+  `,
+  description: css`
+    position: absolute;
+    z-index: 1;
+    opacity: 0;
+    transition: all 500ms ease-out;
+    pointer-events: none;
+    transform: translateY(-50%) rotate(-20deg) scale(1.05);
+  `,
+};
+
 function Contact({ links }) {
-  return <div />;
+  const [currentLink, setCurrentLink] = useState(null);
+
+  const listVariants = {
+    hidden: {
+      x: '-25%',
+      opacity: 0.8,
+    },
+    visible: {
+      x: '0%',
+      opacity: 1,
+      transition: {
+        duration: 850 / 1000,
+        staggerChildren: 85 / 1000,
+        ease: 'easeInOut',
+        delay: 100 / 1000,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    hidden: {
+      x: '-20%',
+    },
+    visible: {
+      x: '0%',
+      transition: { duration: 550 / 1000, ease: [0.25, 0.1, 0.25, 1.0] },
+    },
+  };
+
+  return (
+    <>
+      <motion.ul
+        initial="hidden"
+        animate="visible"
+        variants={listVariants}
+        css={styles.list}
+      >
+        {links.map(({ title, href, color }) => (
+          <motion.li
+            key={title}
+            variants={listItemVariants}
+            css={styles.listItem({
+              linkColor: currentLink?.color,
+              isCurrentLink: currentLink?.title === title,
+            })}
+            onMouseEnter={() => setCurrentLink({ title, color })}
+            onMouseLeave={() => setCurrentLink(null)}
+          >
+            <Link href={href}>{title}</Link>
+          </motion.li>
+        ))}
+      </motion.ul>
+      <div css={styles.listCover} />
+    </>
+  );
 }
 
 export default Contact;
