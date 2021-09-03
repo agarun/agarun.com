a2247a62920396bc9e91db6287baed2fe7e2c9e6
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-03 18:26:24 -0400

    Add icons

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,11 +2,13 @@ import { useRouter } from 'next/router';
 import { NavLink } from '../Nav';
 import Nav from '../Nav';
 import * as styles from './styles';
+import Monogram from '../Monogram';
+import ThemeSwitch from '../ThemeSwitch';
 
 function Layout({ children }) {
   const router = useRouter();
-  const isRootPage = router.pathname.length <= 1;
 
+  const isRootPage = router.pathname.length <= 1;
   if (isRootPage) {
     return <div css={styles.container}>{children}</div>;
   }
@@ -18,10 +20,14 @@ function Layout({ children }) {
           <h1 css={styles.title}>Aaron Agarunov</h1>
         </NavLink>
         <Nav />
+        <ThemeSwitch css={[styles.icon, styles.themeSwitch]} />
       </header>
       <main role="main" css={styles.main}>
         {children}
       </main>
+      <footer>
+        <Monogram css={[styles.icon, styles.monogram]} />
+      </footer>
     </div>
   );
 }
