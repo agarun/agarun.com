74e3f705499e21b47ad61c329f7384c6b2eff519
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-03 18:24:09 -0400

    Add posts page

---
+++ pages/posts/index.js
@@ -0,0 +1,63 @@
+import { css } from '@emotion/react';
+import { NavLink } from '../../components/Nav';
+import { formatDate } from '../../lib/dates';
+import { getPosts } from '../../lib/posts';
+
+const styles = {
+  title: css`
+    margin: calc(var(--spacing) * 8) 0;
+    font-size: calc(var(--font-size-scale) * 36px);
+    font-weight: 400;
+    line-height: 1.1;
+  `,
+  list: css`
+    margin: calc(var(--spacing) * 8) 0;
+    display: flex;
+    flex-direction: column;
+  `,
+  listItem: css`
+    margin: calc(var(--spacing) * 2) 0;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    font-size: calc(var(--font-size-scale) * 24px);
+    font-weight: var(--font-weight-bold);
+    letter-spacing: -0.5px;
+    border-bottom: 2px solid var(--colors-grey-300);
+  `,
+  date: css`
+    margin-top: var(--spacing);
+    margin-bottom: calc(var(--spacing) * 2);
+    font-size: calc(var(--font-size-scale) * 16px);
+    font-weight: var(--font-weight-body);
+    letter-spacing: normal;
+    color: var(--colors-accent);
+  `,
+};
+
+export async function getStaticProps() {
+  const posts = getPosts();
+  return {
+    props: { posts },
+  };
+}
+
+function Posts({ posts }) {
+  return (
+    <section>
+      <h1 css={styles.title}>
+        A home for personal and technical notes, posts, and essays.
+      </h1>
+      <ul css={styles.list}>
+        {posts.map(({ id, date, title }) => (
+          <li key={id} css={styles.listItem}>
+            <NavLink href={`/posts/${id}`}>{title}</NavLink>
+            <span css={styles.date}>{formatDate(date)}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Posts;
