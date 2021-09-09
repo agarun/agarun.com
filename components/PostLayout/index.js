9b25f942a6d9740a16e17e156754204bf55400f8
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-09 16:23:31 -0400

    Add MDX alternative

--- components/PostLayout/index.js
+++ components/PostLayout/index.js
@@ -1,7 +1,28 @@
+import Image from 'next/image';
+import { MDXRemote } from 'next-mdx-remote';
+import Link from '../../components/Link';
 import PrevNext from './components/PrevNext';
 import Date from './components/Date';
 import Author from './components/Author';
 import * as styles from './styles';
+
+export function Markdown({ html }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} css={styles.prose} />;
+}
+
+export function MDX({ source }) {
+  return (
+    <div css={styles.prose}>
+      <MDXRemote
+        {...source}
+        components={{
+          a: Link,
+          img: Image,
+        }}
+      />
+    </div>
+  );
+}
 
 function Post({ title, date, html, prev, next }) {
   return (
@@ -10,7 +31,7 @@ function Post({ title, date, html, prev, next }) {
       <h2>
         <Date>{date}</Date>
       </h2>
-      <div dangerouslySetInnerHTML={{ __html: html }} css={styles.prose} />
+      <MDX source={html} />
       <Author />
       <PrevNext prev={prev} next={next} />
     </article>
