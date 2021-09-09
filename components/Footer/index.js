b44a7662d99f4223713dccec7d84ae242f4b9a3f
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-09 00:29:20 -0400

    Move icons to footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,26 +1,38 @@
-import { useMemo } from 'react';
 import Monogram from '../Monogram';
 import { getSocials } from '../../lib/socials';
+import GitHubIcon from '../GitHubIcon';
+import LinkedInIcon from '../LinkedInIcon';
+import TwitterIcon from '../TwitterIcon';
 import * as styles from './styles';
 
-function Footer({ ...props }) {
-  const links = useMemo(() => getSocials().filter((link) => link.footer), []);
+const icons = {
+  GitHub: GitHubIcon,
+  LinkedIn: LinkedInIcon,
+  Twitter: TwitterIcon,
+};
 
+const links = getSocials().filter((link) => link.footer);
+
+function Footer({ ...props }) {
   return (
     <footer css={styles.footer} {...props}>
       <div css={styles.content}>
         <span>© Aaron Agarunov</span>
-        {links.map((link) => (
-          <a
-            key={link.title}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={link.href}
-            css={styles.link}
-          >
-            <link.icon width={14} height={14} />
-          </a>
-        ))}
+        {links.map((link) => {
+          const Icon = icons[link.title];
+
+          return (
+            <a
+              key={link.title}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={link.href}
+              css={styles.link}
+            >
+              <Icon width={14} height={14} />
+            </a>
+          );
+        })}
         <a href="#top" css={styles.link}>
           <span role="img" aria-label="arrow pointing to top of page">
             ↑
