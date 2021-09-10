b3f6b7142a8185f1e21a8b9ff2632b048378d66a
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-10 13:36:19 -0400

    Add Jest setup file

---
+++ tests/setup.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect';
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
