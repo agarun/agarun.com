d53da7920961718f5977823217247c909faf2b5f
Aaron Agarunov <agarunov.aaron@gmail.com>
2021-09-11 18:51:08 -0400

    Add recent projects to homepage, add entry animations for each section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
+import { getProjects } from '../lib/projects';
 import Link from '../components/Link';
 import Monogram from '../components/Monogram';
 import ThemeSwitch from '../components/ThemeSwitch';
 import Subtitle from '../components/Subtitle';
+import Recent from '../components/Recent';
 import { NavLink } from '../components/Nav';
 
 const styles = {
@@ -35,33 +39,78 @@ const styles = {
   `,
 };
 
-export default function Home() {
+export async function getStaticProps() {
+  const projects = getProjects();
+  const recentProjects = projects.slice(0, 2);
+  return { props: { recentProjects } };
+}
+
+function Home({ recentProjects }) {
+  /**
+   * using `will-change` appears to help `transform` performance
+   * here for `x` & `y` transitions.
+   * "it is a good practice to switch will-change on and off using
+   * script code before and after the change occurs."
+   * -- https://developer.mozilla.org/en-US/docs/Web/CSS/will-change
+   */
+  const [hintStyle, setHintStyle] = useState({ willChange: 'transform' });
+  useEffect(() => {
+    const timeout = setTimeout(() => setHintStyle({}), 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const topMotionProps = {
+    style: hintStyle,
+    initial: { opacity: 0, y: 'calc(var(--spacing) * -0.5)' },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.6 },
+  };
+
+  const middleMotionProps = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { duration: 0.5, delay: 0.3 },
+  };
+
+  const bottomMotionProps = {
+    style: hintStyle,
+    initial: { opacity: 0, y: 'var(--spacing)' },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.6, delay: 0.6 },
+  };
+
   return (
     <>
       <Head>
         <title>Aaron Agarunov</title>
       </Head>
-      <header css={styles.header}>
+      <motion.header css={styles.header} {...topMotionProps}>
         <h1 css={styles.heading}>Aaron Agarunov</h1>
         <ThemeSwitch />
-      </header>
+      </motion.header>
       <main role="main">
-        <p css={styles.subheading}>
+        <motion.p css={styles.subheading} {...middleMotionProps}>
           Software developer based in Brooklyn, NY. Building web experiences at{' '}
           <Link href="https://mskcc.org">MSKCC</Link>.
-        </p>
-        <p css={styles.description}>
-          I&apos;m interested in user interfaces, data visualization, and
-          interaction design. <NavLink href="/about">Read more</NavLink>, see my{' '}
-          <NavLink href="/projects">projects</NavLink>,{' '}
-          <NavLink href="/posts">posts</NavLink>, or{' '}
-          <NavLink href="/contact">contact info</NavLink>.
-        </p>
-        <Subtitle>Projects</Subtitle>
+        </motion.p>
+        <motion.div {...bottomMotionProps}>
+          <p css={styles.description}>
+            I work on user interfaces and data visualizations. I focus on the
+            details to make apps simple. tODO-Change
+            <NavLink href="/about">Read more</NavLink>, see my{' '}
+            <NavLink href="/projects">projects</NavLink>,{' '}
+            <NavLink href="/posts">posts</NavLink>, or{' '}
+            <NavLink href="/contact">contact info</NavLink>.
+          </p>
+          <Subtitle>Projects</Subtitle>
+          <Recent projects={recentProjects} />
+        </motion.div>
       </main>
-      <footer css={styles.footer}>
+      <motion.footer {...bottomMotionProps} css={styles.footer}>
         <Monogram width={30} height={30} />
-      </footer>
+      </motion.footer>
     </>
   );
 }
+
+export default Home;
