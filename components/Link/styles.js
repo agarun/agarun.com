130a9832ff483c1bc437742ceef6236a83c5745b
Aaron Agarunov <agarunov.aaron@gmail.com>
2025-03-27 17:44:01 -0400

    Update link styles

--- components/Link/styles.js
+++ components/Link/styles.js
@@ -2,24 +2,26 @@ import { css } from '@emotion/react';
 
 export const link = css`
   display: inline-block;
-  text-decoration: none;
+  text-decoration: underline;
+  text-decoration-color: hsl(250deg, 79%, 63%, 0.75);
   text-shadow: var(--text-shadow);
-  color: var(--colors-grey-800);
+  color: var(--colors-text-primary);
   background: linear-gradient(
     20deg,
     var(--colors-link-700) 0%,
     var(--colors-link-gradient) 100%
   );
   background-repeat: repeat-x;
-  background-position: 0px 82%;
-  background-size: 100% 44%;
+  background-position: 0px 92%;
+  background-size: 100% 6px;
   transition: all 100ms ease-out;
   cursor: pointer;
 
   &:hover {
-    color: var(--colors-link);
-    background-position: 0px 100%;
-    background-size: 100% 3px;
+    color: hsl(250, 100%, 9%);
+    text-decoration: none;
+    background-position: 0px 60%;
+    background-size: 100% 50%;
     transform: translateY(-2px) scaleX(0.98) scaleY(0.98);
   }
 `;
