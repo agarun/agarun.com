7110fa5ada536f38a152f2d13f42a434e6d7c43b
Aaron Agarunov <agarunov.aaron@gmail.com>
2025-04-24 09:57:26 -0400

    Add icons

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -1,5 +1,7 @@
 export { default as ArrowTopIcon } from './ArrowTopIcon';
 export { default as DropletIcon } from './DropletIcon';
+export { default as PaletteIcon } from './PaletteIcon';
+export { default as PaintIcon } from './PaintIcon';
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as TwitterIcon } from './TwitterIcon';
 export { default as LinkedInIcon } from './LinkedInIcon';
